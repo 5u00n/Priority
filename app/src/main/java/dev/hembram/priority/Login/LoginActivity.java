f2f5808f67bec4ad0ede48397ee104cde71a5773
5u00n <surenhembram97@gmail.com>
2023-09-22 03:14:13 +0530

    Initial commit

---
+++ app/src/main/java/dev/hembram/priority/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package dev.hembram.priority.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.hembram.priority.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/dev/hembram/priority/MainActivity.java
@@ -0,0 +1,19 @@
+package dev.hembram.priority;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import dev.hembram.priority.Login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
